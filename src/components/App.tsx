2b1e4229c0cb5f990d8cf92b0933358169506ae8
kleva-j <kasmickleva@gmail.com>
2023-08-04 13:52:01 +0100

    chore: setup fonts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 
+import { Button } from '@/components/ui/button';
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <button type="button" onClick={() => setCount((count) => count + 1)}>
-        Count is: {count}
-      </button>
-    </div>
+    <Button type="button" onClick={() => setCount((count) => count + 1)}>
+      Count is: {count}
+    </Button>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import '@/styles/index.css';
+import '@fontsource/inter';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
