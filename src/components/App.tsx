00bb217d825bc58e091d3fd59e20dc789e175e82
kleva-j <kasmickleva@gmail.com>
2023-08-06 00:41:16 +0100

    feat: setup error boundary

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,16 @@
+import { ErrorBoundary } from 'react-error-boundary';
+
 import { Footer } from '@/components/layout/Footer';
 import { Header } from '@/components/layout/Header';
 
 function App() {
   return (
-    <section className="">
-      <Header />
-      <Footer />
-    </section>
+    <ErrorBoundary fallback={<div>Something went wrong</div>}>
+      <section className="">
+        <Header />
+        <Footer />
+      </section>
+    </ErrorBoundary>
   );
 }
 
