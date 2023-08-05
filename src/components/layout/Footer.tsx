ee864816d1536e97f5d1d788f3eed264747df6bc
kleva-j <kasmickleva@gmail.com>
2023-08-06 00:02:40 +0100

    chore: loading mono fonts

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+export const Footer: FC = () => {
+  return (
+    <footer className="fixed bg-[#0a182ed9] text-center py-4 bottom-0 w-full font-mono text-[13px] text-teal-500">
+      Built by kasmickleva
+    </footer>
+  );
+};
