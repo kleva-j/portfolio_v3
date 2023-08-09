51905e5adca40e3c14c07a7f60cd0daf0c9c4f7f
kleva-j <kasmickleva@gmail.com>
2023-08-09 20:58:20 +0100

    feat: implement left and right floating bars

---
+++ src/components/RightFloatBar.tsx
@@ -0,0 +1,12 @@
+export const RightFloatBar = () => (
+  <div className="hidden md:block fixed right-5 lg:right-10 left-auto w-10 bottom-0 text-[#a8b2d1] z-10 font-mono text-[13px]">
+    <div className=" flex flex-col justify-center gap-y-8 after:relative after:content-[''] after:block after:w-px after:h-[90px] after:mx-auto after:bg-[#a8b2d1]">
+      <a
+        href="mailto:kasmickleva@gmail.com"
+        className="vertical-lr mx-auto hover:text-teal-500 hover:-translate-y-[3px] transition-transform duration-300 tracking-widest"
+      >
+        kasmickleva@gmail.com
+      </a>
+    </div>
+  </div>
+);
