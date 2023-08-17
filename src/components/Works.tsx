42d30ffdcae44c75da3a2a4f065613b7b8afcf19
kleva-j <kasmickleva@gmail.com>
2023-08-17 15:36:02 +0100

    feat: minor fixes

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -3,7 +3,7 @@ import { SectionWrapper } from '@/components/hoc/SectionWrapper';
 import { ProjectList } from '@/components/ProjectList';
 import { useFetch } from '@/libs/hooks';
 
-const url = import.meta.env.VITE_API_URL ?? 'http://localhost:3002/api/Projects/';
+const url = `${import.meta.env.VITE_API_URL}/api/Projects/`;
 
 export const Works = SectionWrapper(
   () => {
