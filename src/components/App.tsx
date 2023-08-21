44af78cc78526c0cfdfe9fec513e3aad4588d44f
kleva-j <kasmickleva@gmail.com>
2023-08-21 11:02:01 +0100

    feat: implement responsive sidebar nav component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,54 @@
+import { useCycle } from 'framer-motion';
+import { motion } from 'framer-motion';
+import { createContext, useContext } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 
+import { About } from '@/components/About';
+import { Contact } from '@/components/Contact';
+import { Experience } from '@/components/Experience';
+import { Hero } from '@/components/Hero';
+import { Footer } from '@/components/layout/Footer';
 import { Header } from '@/components/layout/Header';
+import { SideNav } from '@/components/layout/sidenav';
 import { LeftFloatBar } from '@/components/LeftFloatBar';
-import { MainContent } from '@/components/MainContent';
 import { RightFloatBar } from '@/components/RightFloatBar';
+import { Works } from '@/components/Works';
+
+export interface IAppContext {
+  theme: string;
+  toggleTheme: () => void;
+  isOpen: boolean;
+  toggleOpen: () => void;
+}
+
+const AppContext = createContext<IAppContext | null>(null);
+
+export function UseAppContext() {
+  const context = useContext(AppContext);
+  if (context === undefined) throw new Error(`UseTAppContext must be used within a ContextProvider`);
+  return context;
+}
 
 function App() {
+  const [isOpen, toggleOpen] = useCycle(false, true);
+  const [theme, toggleTheme] = useCycle('light', 'dark');
+
   return (
     <ErrorBoundary fallback={<div>Something went wrong</div>}>
-      <Header />
-      <MainContent />
+      <AppContext.Provider value={{ toggleOpen, isOpen, theme, toggleTheme }}>
+        <Header />
+        <SideNav toggleOpen={toggleOpen} isOpen={isOpen} />
+        <motion.section className={isOpen ? 'max-sm:blur-sm max-sm:brightness-75' : ''}>
+          <main className="max-w-[1600px] min-h-screen mx-auto px-6 sm:px-[50px] md:px-[100px] lg:px-[150px] items-center justify-center bg-[#0a182e] [counter-reset:section]">
+            <Hero />
+            <About />
+            <Experience />
+            <Works />
+            <Contact />
+          </main>
+          <Footer />
+        </motion.section>
+      </AppContext.Provider>
       <LeftFloatBar />
       <RightFloatBar />
     </ErrorBoundary>
