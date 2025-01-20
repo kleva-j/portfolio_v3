fc5e2ad50b49f3eebeb6b73f24fc9db09c9f6cf7
kleva-j <kasmickleva@gmail.com>
2025-01-20 16:04:41 +0100

    fix: fix app context webpack error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import '../styles/logo.css';
 
 import type { Metadata } from 'next';
 
-import { AppContextProvider } from '@/components/context/AppContext';
 import { inter, sfMono } from '@/lib/utils';
 
 export const metadata: Metadata = {
@@ -16,7 +15,6 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <html lang="en">
       <body className={`${inter.variable} ${sfMono.variable} font-sans`}>
-        <AppContextProvider>{children}</AppContextProvider>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
+"use client";
+
+import { AppContextProvider } from '@/components/context/AppContext';
 import { RightFloatBar } from '@/components/RightFloatBar';
 import { LeftFloatBar } from '@/components/LeftFloatBar';
 import { MainSection } from '@/components/MainSection';
 import { SideNav } from '@/components/layout/sidenav';
 import { Header } from '@/components/layout/Header';
-import { Fragment } from 'react';
 
 export default function HomePage() {
   return (
-    <Fragment>
+    <AppContextProvider>
       <Header />
       <SideNav />
       <MainSection />
       <RightFloatBar />
       <LeftFloatBar />
-    </Fragment>
+    </AppContextProvider>
   )
 }
