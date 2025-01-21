6637c8a73c74c33dbf54d97941d7d6ed55ba0304
kleva-j <kasmickleva@gmail.com>
2025-01-21 13:17:38 +0100

    chore: remove redundant files

--- src/libs/hooks/index.ts
+++ src/libs/hooks/index.ts
@@ -1,6 +1,4 @@
-export { useDimensions } from './useDimension';
 export { useEventListener } from './useEventListener';
-export { useFetch } from './useFetch';
 export { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
 export { useOnClickOutside } from './useOnClickOutside';
 export { useElementSize } from './useSize';
