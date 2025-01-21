27063fc34ae2e5a1d6dda1f5b61198faa5323663
kleva-j <kasmickleva@gmail.com>
2025-01-21 13:20:50 +0100

    feat: add local source for projects

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,25 +1,21 @@
 import { FeaturedCard } from '@/components/FeaturedCard';
 import { SectionWrapper } from '@/components/hoc/SectionWrapper';
 import { ProjectList } from '@/components/ProjectList';
-import { useFetch } from '@/libs/hooks';
-
-const url = import.meta.env.VITE_API_URL;
+import { projects } from '@/libs/utils/constants';
 
 export const Works = SectionWrapper(
   () => {
-    const { data: works } = useFetch(url + '/api/Projects/');
-
     return (
       <div>
         <h2 className="relative flex items-center before:relative [counter-increment:section] before:[content:'0'counter(section)'.'] before:text-teal-500 before:mr-[5px] mt-[10px] mb-[40px] whitespace-nowrap text-2xl before:font-mono font-semibold text-[#ccd6f6] after:content-[''] after:ml-[10px] after:w-full after:max-w-[300px] after:h-px after:relative after:block after:bg-[#233554]">
           Some Things I&rsquo;ve Built
         </h2>
         <ul>
-          {works.slice(0, 3).map((item) => (
+          {projects.slice(0, 3).map((item) => (
             <FeaturedCard key={item.title} {...item} />
           ))}
         </ul>
-        <ProjectList works={works} />
+        <ProjectList works={projects} />
       </div>
     );
   },
